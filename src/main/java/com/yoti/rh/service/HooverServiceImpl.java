3d53800181255034d409b4d0eeac70419ccda7d5
alastair <alastair.knowles@tide.co>
2018-03-07 18:20:42 +0000

    wrote the algorithm to drive the hoover - straightforward

--- src/main/java/com/yoti/rh/service/HooverServiceImpl.java
+++ src/main/java/com/yoti/rh/service/HooverServiceImpl.java
@@ -4,7 +4,14 @@ import com.yoti.rh.domain.HooveringEvent;
 import com.yoti.rh.dto.HooverInputDto;
 import com.yoti.rh.dto.HooverOutputDto;
 import com.yoti.rh.repository.HooverRepository;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.yoti.rh.util.CoordinateUtils.isInBounds;
 
 @Service
 public class HooverServiceImpl implements HooverService {
@@ -25,13 +32,72 @@ public class HooverServiceImpl implements HooverService {
     }
 
     private HooverOutputDto hoover(HooverInputDto input) {
+        Long[] lower = new Long[]{0L, 0L};
+        Long[] upper = input.getRoomSize();
 
+        // Put the dirty patches into an index structure so we can easily find out whether we hoovered them
+        Map<Pair<Long, Long>, Long[]> dirtyPatches = indexDirtyPatches(input);
+        int dirtyPatchesHoovered = 0;
 
-        Long[] startPosition = input.getStartPosition();
+        Long[] position = input.getStartPosition();
+        for (char direction : input.getDirections().toCharArray()) {
+            switch (Direction.valueOf(Character.toString(direction))) {
+                case N:
+                    position = moveNorthIfInBounds(lower, upper, position);
+                    break;
+                case S:
+                    position = moveSouthIfInBounds(lower, upper, position);
+                    break;
+                case E:
+                    position = moveEastIfInBounds(lower, upper, position);
+                    break;
+                case W:
+                    position = moveWestIfInBounds(lower, upper, position);
+                    break;
+            }
 
+            Pair<Long, Long> positionKey = Pair.of(position[0], position[1]);
+            if (dirtyPatches.remove(positionKey) != null) {
+                dirtyPatchesHoovered++;
+            }
+        }
 
-        HooverOutputDto output = new HooverOutputDto();
-        return output;
+        return new HooverOutputDto()
+                .setEndPosition(position)
+                .setDirtyPatchesHoovered(Integer.toUnsignedLong(dirtyPatchesHoovered));
+    }
+
+    private Map<Pair<Long, Long>, Long[]> indexDirtyPatches(HooverInputDto input) {
+        return Stream.of(input.getDirtyPatches())
+                .collect(Collectors.toMap(
+                        dirtyPatch -> Pair.of(dirtyPatch[0], dirtyPatch[1]),
+                        dirtyPatch -> dirtyPatch));
+    }
+
+    private Long[] moveNorthIfInBounds(Long[] lower, Long[] upper, Long[] current) {
+        Long[] next = new Long[]{current[0], current[1] + 1};
+        return isInBounds(lower, upper, next) ? next : current;
+    }
+
+    private Long[] moveSouthIfInBounds(Long[] lower, Long[] upper, Long[] current) {
+        Long[] next = new Long[]{current[0], current[1] - 1};
+        return isInBounds(lower, upper, next) ? next : current;
+    }
+
+    private Long[] moveEastIfInBounds(Long[] lower, Long[] upper, Long[] current) {
+        Long[] next = new Long[]{current[0] + 1, current[1]};
+        return isInBounds(lower, upper, next) ? next : current;
+    }
+
+    private Long[] moveWestIfInBounds(Long[] lower, Long[] upper, Long[] current) {
+        Long[] next = new Long[]{current[0] - 1, current[1]};
+        return isInBounds(lower, upper, next) ? next : current;
+    }
+
+    private enum Direction {
+
+        N, S, E, W
+
     }
 
 }
